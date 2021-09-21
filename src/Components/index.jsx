dfeddeb72670c5eafc37ca7555ecbbd7cd0edac4
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-09-21 09:35:20 +0200

    deploying with gh-pages

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import NavigationBar from './NavigationBar/index';
 import SideBar from './SideBar/index'
 import BodyContent from './BodyContent/index'
